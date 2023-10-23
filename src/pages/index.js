9e7fb6014a11a5a2e853ecf582dbf6065d8b9cfe
Daniel Medina Cossio <daniel.medina@talent.com>
2023-10-23 00:04:26 -0500

    working on the front of the app

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,29 +29,24 @@ export default function Home() {
 
   return (
     <>
-      <h1 className="text-5xl text-center text-black">TEXT ANALYZER</h1>
+      <h1 className="text-5xl text-center text-white font-bold">TEXT ANALYZER</h1>
       <div className="flex justify-center mt-4">
-        <form onSubmit={handleSubmit} className="flex-col">
-          <label>
-            Introduce un texto:
-           
-           
-          </label>
+        <form onSubmit={handleSubmit} className="flex flex-col content-center">
           <textarea
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-              rows="4"
-              cols="50"
-            ></textarea>
-          
-          <button type="submit">Enviar</button>
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            rows="7"
+            cols="80"
+            className="bg-stone-600 rounded text-white p-4 text-lg"
+          ></textarea>
+          <button type="submit"
+          className="mt-4 text-stone-400 hover:text-stone-950 border border-stone-400 hover:bg-stone-400 focus:ring-4 focus:outline-none focus:ring-stone-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-fit"
+          >Submit</button>
         </form>
       </div>
       {data && (
         <>
-        <pre>
-          {JSON.stringify(data, null, 2)}
-        </pre>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
         </>
       )}
     </>
