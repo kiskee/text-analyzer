ba234d2702b878b36eacf164322c21cdd1edeeed
Daniel Medina Cossio <daniel.medina@talent.com>
2023-10-22 23:49:03 -0500

    adding the front

--- backend/sentimentApi/functions/sentimentAnalysis.js
+++ backend/sentimentApi/functions/sentimentAnalysis.js
@@ -33,6 +33,10 @@ module.exports.sentimentAnalysis = async (event) => {
 
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
       body: JSON.stringify(response),
     };
   } catch (e) {
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
 import '@/styles/globals.css'
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <Head>
+        <title>Text Analizer</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content=""
+          key="desc"
+        />
+        <meta name="robots" content="all" />
+      </Head>
+      <div className="py-12 px-16 bg-stone-800">
+        <Component {...pageProps} />
+      </div>
+      
+    </>
+  )
+  }
